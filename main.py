6e799ef1e1e268d270bd7e9c9347f60ac4da5715
shamalkale2007-jpg <shamalkale2007@gmail.com>
2026-02-01 23:44:41 +0530

    Refactor task management code for clarity

--- main.py
+++ main.py
@@ -6,32 +6,20 @@ print("--- Task List Manager ---")
 tasks = ["Sleep", "Getup", "Brush"]
 print(f"Original Tasks: {tasks}")
 
-# Adding a new task
-new_task = input("Enter the new task to add:")
-tasks.append(new_task)
-print("Tasks after adding:", tasks)
-
-#Editing a task
-edit_index = int(input("Enter the index of the task to edit:"))
-new_task_name = input("Enter the new task:")
-tasks[edit_index] = new_task_name 
-print("Task after Editing:", tasks)
-
-#Removing a task
-remove_index = int(input("Enter the index of the task to remove:"))
-tasks.pop(remove_index)
-print("Tasks after Removing:", tasks)
-
-#Sorting tasks
-tasks.sort()
-print("Tasks after Sorting:", tasks) write your code here
+# Write your code here
 # TODO: Add & Print new Task from user
-
+new_task = input("Enter a new task to add: ")
+tasks.append(new_task)
+print(f"Tasks after Adding: {tasks}")
 # TODO: Edit & Print task selected by User
-
+edit_index = int(input("Enter the index of the task to edit: "))
+edited_task = input("Enter the new task: ")
+tasks[edit_index] = edited_task
+print(f"Tasks after Editing: {tasks}")
 # TODO: Remove & Print a Task selected by User
-
+remove_index = int(input("Enter the index of the task to remove: "))
+tasks.pop(remove_index)
+print(f"Tasks after Removing: {tasks}")
 # TODO: Sort & Print the Tasks
-
-
-
+tasks.sort()
+print(f"Tasks after Sorting: {tasks}")
