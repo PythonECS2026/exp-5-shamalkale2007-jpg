59972f1229a5453ed5088007e8d2bd37ab878d3a
shamalkale2007-jpg <shamalkale2007@gmail.com>
2026-02-01 23:06:40 +0530

    Implement task management features in main.py

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 # AIM: Task List Manager
-# Coder:
-# Date:
+# Coder:Shamal 
+# Date:26/01/26
 
 print("--- Task List Manager ---")
 tasks = ["Sleep", "Getup", "Brush"]
 print(f"Original Tasks: {tasks}")
 
-# Write your code here
+# Adding a new task
+new_task = input("Enter the new task to add:")
+tasks.append(new_task)
+print("Tasks after adding:", tasks)
+
+#Editing a task
+edit_index = int(input("Enter the index of the task to edit:"))
+new_task_name = input("Enter the new task:")
+tasks[edit_index] = new_task_name 
+print("Task after Editing:", tasks)
+
+#Removing a task
+remove_index = int(input("Enter the index of the task to remove:"))
+tasks.pop(remove_index)
+print("Tasks after Removing:", tasks)
+
+#Sorting tasks
+tasks.sort()
+print("Tasks after Sorting:", tasks) Write your code here
 # TODO: Add & Print new Task from user
 
 # TODO: Edit & Print task selected by User
@@ -14,3 +32,4 @@ print(f"Original Tasks: {tasks}")
 # TODO: Remove & Print a Task selected by User
 
 # TODO: Sort & Print the Tasks
+
