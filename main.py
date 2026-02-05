f2249ccbd4e8bca0d905fe27972355c7f818c6c3
shamalkale2007-jpg <shamalkale2007@gmail.com>
2026-02-05 23:05:51 +0530

    Refactor task manager to include user input features

--- main.py
+++ main.py
@@ -2,24 +2,29 @@
 # Coder:Shamal 
 # Date:26/01/26
 
-# Original list
+# Original task list
 tasks = ['Sleep', 'Getup', 'Brush']
 print("Original Tasks:", tasks)
 
-# Adding a task
-tasks.append('College')
+# --- Add Task ---
+new_task = input("Enter a new task to add: ")
+tasks.append(new_task)
 print("Tasks after Adding:", tasks)
 
-# Editing a task
-tasks[1] = 'Get Up'
+# --- Edit Task ---
+edit_index = int(input("Enter the index of the task to edit: "))
+new_value = input("Enter the new task: ")
+tasks[edit_index] = new_value
 print("Tasks after Editing:", tasks)
 
-# Removing a task
-tasks.remove('Sleep')
+# --- Remove Task ---
+remove_index = int(input("Enter the index of the task to remove: "))
+tasks.pop(remove_index)
 print("Tasks after Removing:", tasks)
 
-# Sorting tasks
+# --- Sort Tasks ---
 tasks.sort()
 print("Tasks after Sorting:", tasks)
+
 
 
